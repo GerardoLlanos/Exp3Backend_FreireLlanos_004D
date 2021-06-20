8ed7a7018b8657057e654dd79c0c6f5d00219ed3
gera.llanos <gera.llanos@duocuc.cl>
2021-06-20 15:09:56 -0400

    Se añade el template core de Ver en donde se muestran las zapatillas y una carpeta de imagenes

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, Ver
 
 urlpatterns = [
     path('', home, name="home"),
+    path('ver', Ver, name="ver"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from .models import DatosZapatilla
 
 # Create your views here.
 
 def home(request):
 
     return render(request, 'home.html')
+
+def Ver(request):
+    verzapatillas = DatosZapatilla.objects.all()
+
+    return render(request, 'core/ver.html', context={'verzapatillas':verzapatillas})
